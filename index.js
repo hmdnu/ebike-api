cc666f708ebacb73cd62e78a1d2a3c87cf4b6a69
HamdanUbaidillah <daniuya71@gmail.com>
2023-10-25 09:31:13 +0700

    chore: change env

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
