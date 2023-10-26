8a6545fb7a36d22650e696b2f9013ada4c432fc9
HamdanUbaidillah <daniuya71@gmail.com>
2023-10-26 19:37:07 +0700

    chore: fix origin url

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5000",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
