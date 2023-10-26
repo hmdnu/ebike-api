c35f6ec6b239695cbb69a97ccee34787830e64b9
HamdanUbaidillah <daniuya71@gmail.com>
2023-10-26 14:41:32 +0700

    chore: change cors origin options

--- index.js
+++ index.js
@@ -17,18 +17,9 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(express.json());
 
-app.use(cors());
-
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Credentials", "true");
-  // Add other required headers here
-
-  if (req.method === "OPTIONS") {
-    // Handle preflight requests (OPTIONS)
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    return res.status(204).end();
-  }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   next();
 });
