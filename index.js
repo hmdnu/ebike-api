8665decf8a2084a37482658c76dc94c4a51df9f1
HamdanUbaidillah <daniuya71@gmail.com>
2023-10-28 09:04:52 +0700

    change client url

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ dotenv.config();
 import userRoutes from "./routes/User.js";
 import rentalRoutes from "./routes/Rental.js";
 import stationRoutes from "./routes/Station.js";
-
 import Station from "./models/Station.js";
 
 const app = express();
@@ -17,7 +16,12 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: "GET,POST,PUT,DELETE",
+  })
+);
 
 // database connection
 mongoose
